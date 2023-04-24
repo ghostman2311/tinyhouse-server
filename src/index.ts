d086b09379ee16996c89266134ef6e24807f6028
Nikhil <nkarnwal07@gmail.com>
2023-04-24 22:29:33 +0530

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,7 @@
 import express from "express";
-import { ApolloServer } from "apollo-server-express";
-import { schema } from "./graphql";
 
 const app = express();
 const port = 9000;
-
-const server = new ApolloServer({ schema });
-server.start().then(() => {
-  server.applyMiddleware({ app, path: "/api" });
-});
 
 app.listen(port);
 
