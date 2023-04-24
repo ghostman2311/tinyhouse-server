2ff1a1c32dce9fbfe55216762dad3680c6efa404
Nikhil <nkarnwal07@gmail.com>
2023-04-24 22:28:21 +0530

    23 april creating graphql schema

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { schema } from "./graphql";
+
+const app = express();
+const port = 9000;
+
+const server = new ApolloServer({ schema });
+server.start().then(() => {
+  server.applyMiddleware({ app, path: "/api" });
+});
+
+app.listen(port);
+
+console.log(`[app] : http://localhost:${port}`);
